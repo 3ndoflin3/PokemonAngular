603f6ffeb29aa6d1466d792b879f933f92b01f44
3ndoflin3 <kirylbilbao@hotmail.com>
2020-02-13 11:56:15 +0100

    first commit

---
+++ src/app/paginas/inicio/inicio.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pokemon } from 'src/app/model/pokemon';
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss']
+})
+export class InicioComponent implements OnInit {
+
+  listaPokemon: Array<Pokemon>;
+  pokemonSeleccionado: Pokemon;
+  habilidades: Set<any>;
+  hayPokemon: boolean;
+
+  constructor(private pokemonService: PokemonService) {
+    console.trace('InicioComponent constructor')
+    this.listaPokemon = new Array<Pokemon>();
+    this.habilidades = new Set<any>();
+
+  }//constructor()
+
+  ngOnInit() {
+    console.trace('InicioComponent ngOnInit');
+
+    //Mostramos el listado de pokemon
+    this.obtenerListado();
+
+
+  }//ngOnInit
+
+  comprobarPokemon(pokemon) {
+    if (pokemon != '' || typeof pokemon != undefined) {
+      this.hayPokemon = true;
+    };
+    return this.hayPokemon;
+  }
+
+  //Método para obtener todos los pokemon
+  obtenerListado() {
+    this.pokemonService.getAll().subscribe(
+
+      data => {
+        console.debug(data);
+        this.listaPokemon = data;
+
+        /* data.forEach(habilidad => {
+          console.log(habilidad);
+          return this.habilidades = data.map(habilidad => habilidad.nombre);
+
+        }); */
+
+        console.debug('habilidades %o', this.habilidades)
+      },
+
+      error => {
+        console.warn('Error al obtener el listado de pokemon');
+      },
+      () => {
+        console.trace('Estamos intentando obtener el listado');
+      }
+    )
+  }//obtenerListado()
+
+
+
+  seleccionarPokemon(pokemon) {
+    console.log('Click seleccionarPokemon');
+    this.pokemonSeleccionado = pokemon;
+
+  }//seleccionarPokemon
+
+}//InicioComponent
